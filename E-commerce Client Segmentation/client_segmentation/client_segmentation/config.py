7162ab2d27ba4b12de075f19c2ebcc42f0bc09ac
gasobral <gasobral@gmail.com>
2025-03-18 16:49:49 -0300

    Reestruturando o projeto

--- E-commerce Client Segmentation/client_segmentation/client_segmentation/config.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-
-data_path = {
-    'raw' : Path('../../data/raw/'),
-    'processed' : Path('../../data/processed/'),
-    'cleaned': Path('../../data/cleaned/')
-}
--- E-commerce Client Segmentation/client_segmentation/setup.py
+++
@@ -1,4 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(name='client_segmentation',
-      version='0.1')
