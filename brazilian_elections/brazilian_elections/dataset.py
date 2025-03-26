5b2352c1442e338234305fcf59033ee4c6453c9b
gasobral <gasobral@gmail.com>
2025-03-26 16:58:40 -0300

    Fixed broken paths in notebook

--- brazilian_elections/brazilian_elections/dataset.py
+++ brazilian_elections/brazilian_elections/dataset.py
@@ -1,28 +1,54 @@
 from pathlib import Path
+import zipfile
 
 from loguru import logger
 from tqdm import tqdm
 import typer
 
-from brazilian_elections.config import PROCESSED_DATA_DIR, RAW_DATA_DIR
+try:
+    from config import PROCESSED_DATA_DIR
+    from config import RAW_DATA_DIR
+
+except ModuleNotFoundError:
+    from brazilian_elections.config import PROCESSED_DATA_DIR
+    from brazilian_elections.config import RAW_DATA_DIR
 
 app = typer.Typer()
 
 
 @app.command()
 def main(
-    # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
-    input_path: Path = RAW_DATA_DIR / "dataset.csv",
-    output_path: Path = PROCESSED_DATA_DIR / "dataset.csv",
-    # ----------------------------------------------
+    input_path: Path = RAW_DATA_DIR,
+    output_path: Path = PROCESSED_DATA_DIR,
 ):
-    # ---- REPLACE THIS WITH YOUR OWN CODE ----
+
     logger.info("Processing dataset...")
-    for i in tqdm(range(10), total=10):
-        if i == 5:
-            logger.info("Something happened for iteration 5.")
+
+    # creates a list of files to be processed
+    input_files = tqdm(input_path.iterdir())
+
+    for curr_file in input_files:
+        logger.info(f"Processing the file {curr_file}.")
+
+        if curr_file.name.startswith('.'):
+            logger.warning(f"Ignoring hiddne file {curr_file.name}")
+
+        else:
+            if curr_file.suffix == '.zip':
+                logger.info(f"File {curr_file.name} is compressed.")
+
+                with zipfile.ZipFile(curr_file) as zip_ref:
+                    logger.info(f"Decompressing the file {curr_file.name}")
+                    zip_ref.extractall(PROCESSED_DATA_DIR)
+
+                logger.info(f"Extracted file {curr_file.name} into "
+                            f"directory {PROCESSED_DATA_DIR}")
+            else:
+                curr_file.rename(PROCESSED_DATA_DIR / curr_file.name)
+                logger.info(f"Moved the file {curr_file.name} into "
+                            f"directory {PROCESSED_DATA_DIR}.")
+
     logger.success("Processing dataset complete.")
-    # -----------------------------------------
 
 
 if __name__ == "__main__":
