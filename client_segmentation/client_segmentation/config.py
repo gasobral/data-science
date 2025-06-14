7a64db8fef25f085ac2caa0f2d8acd521075e725
gasobral <gasobral@gmail.com>
2025-06-14 14:08:57 -0300

    Implemented the function to retreive data

--- client_segmentation/client_segmentation/config.py
+++ client_segmentation/client_segmentation/config.py
@@ -31,3 +31,98 @@ try:
 
 except ModuleNotFoundError:
     pass
+
+
+## below there is a mapping of dtypes for every column of each csv file
+## data_mapping is a dictionary which its key = csv file and
+## value = dict (dtype mapping, where key = column and value = dtype)
+product_category_name_translation_map = {
+    'product_category_name' : 'string',
+    'product_category_name_english' : 'string'
+}
+
+olist_sellers_dataset_map = {
+    'seller_id': 'string',
+    'seller_zip_code_prefix': 'int64',
+    'seller_city': 'string',
+    'seller_state': 'string'
+}
+
+olist_geolocation_dataset_map = {
+    'geolocation_zip_code_prefix': 'int64',
+    'geolocation_lat': 'float64',
+    'geolocation_lng': 'float64',
+    'geolocation_city': 'string',
+    'geolocation_state': 'string'
+}
+
+olist_products_dataset_map = {
+    'product_id': 'string',
+    'product_category_name': 'string',
+    'product_name_lenght': 'float64',
+    'product_description_lenght': 'float64',
+    'product_photos_qty':  'float64',
+    'product_weight_g': 'float64',
+    'product_length_cm': 'float64',
+    'product_height_cm': 'float64',
+    'product_width_cm': 'float64'
+}
+
+olist_order_items_dataset_map = {
+    'order_id': 'string',
+    'order_item_id': 'int64',
+    'product_id': 'string',
+    'seller_id': 'string',
+    'shipping_limit_date': 'object',
+    'price': 'float64',
+    'freight_value': 'float64'
+}
+
+olist_order_payments_dataset_map = {
+    'order_id': 'string',
+    'payment_sequential': 'int64',
+    'payment_type': 'string',
+    'payment_installments': 'int64',
+    'payment_value': 'float64',
+}
+
+olist_orders_dataset_map = {
+    'order_id': 'string',
+    'customer_id': 'string',
+    'order_status': 'string',
+    'order_purchase_timestamp': 'object',
+    'order_approved_at': 'object',
+    'order_delivered_carrier_date': 'object',
+    'order_delivered_customer_date': 'object',
+    'order_estimated_delivery_date': 'object'
+}
+
+olist_order_reviews_dataset_map = {
+    'review_id': 'string',
+    'order_id': 'string',
+    'review_score': 'int64',
+    'review_comment_title': 'string',
+    'review_comment_message': 'string',
+    'review_creation_date': 'object',
+    'review_answer_timestamp': 'object'
+}
+
+olist_customers_dataset_map = {
+     'customer_id': 'string',
+     'customer_unique_id': 'string',
+     'customer_zip_code_prefix': 'int64',
+     'customer_city': 'string',
+     'customer_state': 'string'
+}
+
+data_mapping = {
+    'product_category_name_translation': product_category_name_translation_map,
+    'olist_sellers_dataset': olist_sellers_dataset_map,
+    'olist_geolocation_dataset': olist_geolocation_dataset_map,
+    'olist_products_dataset': olist_products_dataset_map,
+    'olist_order_items_dataset': olist_order_items_dataset_map,
+    'olist_order_payments_dataset': olist_order_payments_dataset_map,
+    'olist_orders_dataset': olist_orders_dataset_map,
+    'olist_order_reviews_dataset': olist_order_reviews_dataset_map,
+    'olist_customers_dataset': olist_customers_dataset_map
+}
--- client_segmentation/client_segmentation/dataset.py
+++ client_segmentation/client_segmentation/dataset.py
@@ -1,16 +1,92 @@
 from pathlib import Path
+import zipfile
 
 from loguru import logger
 from tqdm import tqdm
 import typer
 
-import zipfile
+from sklearn.preprocessing import OrdinalEncoder
+from sklearn.preprocessing import RobustScaler
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.pipeline      import Pipeline
+from sklearn.compose       import ColumnTransformer
 
 from client_segmentation.config import PROCESSED_DATA_DIR
 from client_segmentation.config import RAW_DATA_DIR
 from client_segmentation.config import INTERIM_DATA_DIR
 
+## imports the data mapping for the csv files from Olist data set
+from client_segmentation.config import olist_customers_dataset_map
+from client_segmentation.config import olist_order_reviews_dataset_map
+from client_segmentation.config import olist_order_reviews_dataset_map
+from client_segmentation.config import olist_orders_dataset_map
+from client_segmentation.config import olist_order_payments_dataset_map
+from client_segmentation.config import olist_order_items_dataset_map
+from client_segmentation.config import olist_products_dataset_map
+from client_segmentation.config import olist_geolocation_dataset_map
+from client_segmentation.config import olist_sellers_dataset_map
+from client_segmentation.config import product_category_name_translation_map
+from client_segmentation.config import data_mapping
+
 app = typer.Typer()
+
+## Functions - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+def retrieve_dataset(input_path: Path = RAW_DATA_DIR) -> int:
+    """
+Retrives the Olist data set from Kaggle, if the csv files are not at
+raw directory.
+
+Arguments
+---------
+input_path: a Path object poting to raw data directory.
+
+Return
+------
+The function returns 1 if any error happens, otherwise, it returns 0.
+    """
+
+    ## retrieves the csv files at raw directory
+    csv_files = [csv_file.name.split('.')[0]
+                 for csv_file in input_path.glob("*.csv")]
+
+    ## check if all required csv files are at raw directory
+    ## note that the keys from the dictionary data_mapping forms
+    ## a list of all csv files from Olist data set
+    csv_files_status = [required_csv in csv_files
+                        for required_csv in data_mapping.keys()]
+
+    ## if all required csv files were found, then no action is required
+    if all(csv_files_status):
+        logger.info("All required csv files are at raw directory. It is"
+                    " not needed to download the data set.")
+        return 0
+
+    ## if any csv file is missing, then the data set is download from
+    ## Kaggle
+    else:
+        logger.info("Some csv files are missing, then the data set"
+                    " will be donwloded from Kaggle.")
+        try:
+            import kagglehub
+
+            dl_path = kagglehub.dataset_download("olistbr/brazilian-ecommerce")
+            logger.success(f"Downloaded the dataset at directory {dl_path}\n")
+            dl_dir = Path(dl_path)
+
+            ## after downloading the data set, move the csv files to
+            ## raw data directory
+            input_files = tqdm(dl_dir.glob("*.csv"))
+
+            for csv_file in input_files:
+                logger.info(f"Moving the file {csv_file} to "
+                            f"{input_path / csv_file.name}\n")
+                csv_file.replace(input_path / csv_file.name)
+
+            return 0
+
+        except Exception as e:
+            print(e)
+            return -1
 
 
 @app.command()
@@ -20,38 +96,14 @@ def main(
     output_path: Path = PROCESSED_DATA_DIR,
 ):
 
-    logger.info("Processing dataset...")
+    if retrieve_dataset(input_path) != 0:
+        logger.error("Failed to download the dataset!\n")
+        return -1
 
-    ## retrives a list of files at raw data directory
-    input_files = tqdm(input_path.iterdir())
+    ## TODO: executar o pré-processanmento
+    ##       mover os arquivos para o diretório intermediário
 
-    ## just moves a file from raw into interim directory
-    ## if it is compressed, then it is decompressed before
-    ## moving it
-    for curr_file in input_files:
-        logger.info(f"Processing the file {curr_file}")
-
-        ## hidden files are ignored
-        if curr_file.name.startswith('.'):
-            logger.warning(f"Ignoring the hidden file {curr_file.name}.")
-
-        else:
-            if curr_file.suffix == '.zip':
-                logger.info(f"File {curr_file.name} is compressed.")
-
-                with zipfile.ZipFile(curr_file) as zip_ref:
-                    logger.info(f"Decompressing the file {curr_file.name}.")
-                    zip_ref.extractall(INTERIM_DATA_DIR)
-
-                logger.info(f"Extracted file {curr_file.name} into "
-                            f"directory {INTERIM_DATA_DIR}")
-
-            else:
-                curr_file.rename(INTERIM_DATA_DIR / curr_file.name)
-                logger.info(f"Moved the file {curr_file.name} into "
-                            f"directory {INTERIM_DATA_DIR}.")
-
-    logger.success("Processing dataset complete.")
+    return 0
 
 
 if __name__ == "__main__":
